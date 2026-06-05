3bf05201cf5602a63c4ac368e0a33e889f4f36d2
abiod <abiod@DESKTOP-5MR3G2M>
2026-06-05 14:15:57 +0100

    Add project files.

---
+++ MICHAEl/Program.cs
@@ -0,0 +1,13 @@
+﻿//Console.WriteLine("Hello, World!");
+Console.WriteLine("list of names and numbers");
+string[] names = { "Michael", "Gabriel", "Tolulope", "Bright", "Sunday", "Joy", "Toyin", "John", "Abiodun", "george", "Chika", "Junior", "Funke", "Funmilayo", "Emmanuel", "Emeka", "Adewale", "Blessing", "Kayode", "Abosede" };
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+for (int i = 0; i <= 50; i += 2)
+{
+    if (i % 2 == 0) ;
+    Console.WriteLine(i);
+}
+Console.ReadLine();
